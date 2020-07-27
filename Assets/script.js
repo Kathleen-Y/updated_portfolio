fbb20e9d9689adf67d251a09342d38abeceeb984
Kathleen Yoannon <k.yoannon@gmail.com>
2020-07-26 20:04:17 -0700

    updated group 2 & group 1 links

--- Assets/script.js
+++ Assets/script.js
@@ -6,8 +6,8 @@
  const slider = document.querySelector('.slider');
  M.Slider.init(slider, {
    indicators: false,
-   height: 500,
-   transition: 500,
+   height: 400,
+   transition: 400,
    interval: 6000
  });
 
@@ -19,4 +19,42 @@
  M.Modal.init(elems, {
 
  });
+ 
+/* creating object of Date class */
+function currentTime() {
+  var date = new Date(); /* creating object of Date class */
+  var hour = date.getHours();
+  var min = date.getMinutes();
+  var sec = date.getSeconds();
+  var midday = "AM";
+  midday = (hour >= 12) ? "PM" : "AM"; /* assigning AM/PM */
+  hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12): hour); /* assigning hour in 12-hour format */
+  hour = updateTime(hour);
+  min = updateTime(min);
+  sec = updateTime(sec);
+  document.getElementById("clock").innerText = hour + " : " + min + " : " + sec + " " + midday; /* adding time to the div */
+    var t = setTimeout(currentTime, 1000); /* setting timer */
+}
 
+function updateTime(k) { /* appending 0 before time elements if less than 10 */
+  if (k < 10) {
+    return "0" + k;
+  }
+  else {
+    return k;
+  }
+}
+
+currentTime();
+
+/* setInterval(function () {
+  var a = moment().toString();
+  $('#clock').text(a)
+}, 1000); */
+
+
+let today = new Date().toLocaleDateString()
+
+console.log(today)
+
+toLocaleDateString();
