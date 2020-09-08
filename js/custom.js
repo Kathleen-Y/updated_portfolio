d0fc29a13f760d249d5aee280f22193962995990
Kathleen Yoannon <k.yoannon@gmail.com>
2020-09-07 18:59:26 -0700

    css and social updates

--- js/custom.js
+++ js/custom.js
@@ -5,7 +5,7 @@ var center;
 function initialize() {
     var mapOptions = {
       zoom: 16,
-      center: new google.maps.LatLng(13.758468, 100.567481),
+      center: new google.maps.LatLng(34.01666652, -118.44572581),
       scrollwheel: false
     };
   
@@ -27,7 +27,7 @@ function calculateCenter() {
 function loadGoogleMap(){
     var script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
+    script.src = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1853.8739113693975!2d-118.445684702015!3d34.01702200243829!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2bb02787166c7%3A0x24783152947f4366!2sSanta%20Monica%20Airport!5e0!3m2!1sen!2sus!4v1599517385352!5m2!1sen!2sus';
     document.body.appendChild(script);
 }
 
