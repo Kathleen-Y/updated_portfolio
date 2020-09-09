453bf2eb528bd55c4e467f348c3be687caed3dd4
Kathleen Yoannon <k.yoannon@gmail.com>
2020-09-08 20:35:52 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
 
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/updated_portfolio", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+//connect to the mongo db
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/updated_portfolio",
+  { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
+);
 
 
